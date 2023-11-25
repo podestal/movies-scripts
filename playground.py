72b78a62171919233b0fb0115143e8c509a03f58
podestal <luisrodriguezpodesta@gmail.com>
2023-11-25 14:48:00 -0500

    init commit

---
+++ playground.py
@@ -0,0 +1,39 @@
+from bs4 import BeautifulSoup
+import requests
+
+########### One Page #########
+# URL = 'https://subslikescript.com/movie/Titanic-120338'
+# response = requests.get(URL)
+# content = response.text
+
+# soup = BeautifulSoup(content, 'lxml')
+# box = soup.find('article', class_='main-article')
+# title = box.find('h1').get_text()
+# tanscrip = box.find('div', class_='full-script').get_text(strip=True, separator=' ')
+
+# with open(f'beautiful-soup/{title}.txt', 'w') as file:
+#     file.write(tanscrip)
+
+########### Multiple Pages #########
+
+ROOT = 'https://subslikescript.com/'
+URL = f'{ROOT}movies'
+response = requests.get(URL)
+content = response.text
+
+soup = BeautifulSoup(content, 'lxml')
+titles = soup.select('ul.scripts-list a')
+links = []
+
+for title in titles:
+    links.append(f'{ROOT}{title.attrs.get('href')}')
+
+for link in links:
+    content = requests.get(link).text
+    soup = BeautifulSoup(content, 'lxml')
+    box = soup.find('article', class_='main-article')
+    title = box.find('h1').get_text()
+    tanscrip = box.find('div', class_='full-script').get_text(strip=True, separator=' ')
+    with open(f'movies-scripts/{title}.txt', 'w') as file:
+        file.write(tanscrip)
+
