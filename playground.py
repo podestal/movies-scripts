cdcf5326f1ee494da64e1f9c049c2469a8a70ea5
podestal <luisrodriguezpodesta@gmail.com>
2023-11-25 15:06:50 -0500

    pagination

--- playground.py
+++ playground.py
@@ -20,20 +20,27 @@ ROOT = 'https://subslikescript.com/'
 URL = f'{ROOT}movies'
 response = requests.get(URL)
 content = response.text
-
 soup = BeautifulSoup(content, 'lxml')
+
+a_tags = soup.select('li.page-item a')
+link_page = a_tags[0].attrs.get('href').split('=')[0]
+for i in range(1, 101):
+    print(f'{link_page}={i}')
+
 titles = soup.select('ul.scripts-list a')
 links = []
 
-for title in titles:
-    links.append(f'{ROOT}{title.attrs.get('href')}')
+# for title in titles:
+#     links.append(f'{ROOT}{title.attrs.get('href')}')
 
-for link in links:
-    content = requests.get(link).text
-    soup = BeautifulSoup(content, 'lxml')
-    box = soup.find('article', class_='main-article')
-    title = box.find('h1').get_text()
-    tanscrip = box.find('div', class_='full-script').get_text(strip=True, separator=' ')
-    with open(f'movies-scripts/{title}.txt', 'w') as file:
-        file.write(tanscrip)
+# for link in links:
+#     content = requests.get(link).text
+#     soup = BeautifulSoup(content, 'lxml')
+#     box = soup.find('article', class_='main-article')
+#     title = box.find('h1').get_text()
+#     tanscrip = box.find('div', class_='full-script').get_text(strip=True, separator=' ')
+#     with open(f'movies-scripts/{title}.txt', 'w') as file:
+#         file.write(tanscrip)
+
+
 
